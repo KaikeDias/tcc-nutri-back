4d19afada3c640b0875767192a9fc91173a16ec3
Kaike <kaikedias98@gmail.com>
2025-01-11 01:17:57 -0300

    fix: fix id type

--- src/main/java/com/tcc2/nutri_app_backend/repositories/PatientRepository.java
+++ src/main/java/com/tcc2/nutri_app_backend/repositories/PatientRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
     Optional<Patient> findByUsername(String username);
 }
